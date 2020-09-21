136f6c551f878f6c7932472d2c0a69c70ad9b66d
Martin Ingram <martin.ingram@gmail.com>
2020-09-21 16:48:45 +1000

    Fix 2020

--- scrape_all.py
+++ scrape_all.py
@@ -1,7 +1,11 @@
 import sys
 from utils import (
-    get_tournament_result_links, get_second_screen_ids, fetch_match_info_json,
-    fetch_second_screen_json, fetch_widget_info_json)
+    get_tournament_result_links,
+    get_second_screen_ids,
+    fetch_match_info_json,
+    fetch_second_screen_json,
+    fetch_widget_info_json,
+)
 from tqdm import tqdm
 from os import makedirs
 from os.path import join, isfile
@@ -12,19 +16,20 @@ year = sys.argv[1]
 
 tournament_links = get_tournament_result_links(year)
 
-target_dir = './results'
+target_dir = "./results"
 
 makedirs(target_dir, exist_ok=True)
 
-match_info_dir = join(target_dir, 'match_info')
-ss_dir = join(target_dir, 'second_screen')
-widget_dir = join(target_dir, 'widget_data')
+match_info_dir = join(target_dir, "match_info")
+ss_dir = join(target_dir, "second_screen")
+widget_dir = join(target_dir, "widget_data")
 
-failed_files = open(join(target_dir, 'failed.txt'), 'w')
+failed_files = open(join(target_dir, "failed.txt"), "w")
 
 makedirs(ss_dir, exist_ok=True)
 makedirs(match_info_dir, exist_ok=True)
 makedirs(widget_dir, exist_ok=True)
+
 
 cache = True
 
@@ -39,27 +44,27 @@ for cur_link in tournament_links:
 
     for cur_ss_id in tqdm(ss_ids):
 
-        cur_identifier = '_'.join(cur_ss_id.values())
+        cur_identifier = "_".join(cur_ss_id.values())
 
-        match_info_file = join(match_info_dir, cur_identifier + '.json')
-        ss_file = join(ss_dir, cur_identifier + '.json')
-        widget_file = join(widget_dir, cur_identifier + '.json')
+        match_info_file = join(match_info_dir, cur_identifier + ".json")
+        ss_file = join(ss_dir, cur_identifier + ".json")
+        widget_file = join(widget_dir, cur_identifier + ".json")
 
         try:
 
             if not isfile(match_info_file) or not cache:
                 match_json = fetch_match_info_json(**cur_ss_id)
-                with open(match_info_file, 'w') as f:
+                with open(match_info_file, "w") as f:
                     json.dump(match_json, f)
 
             if not isfile(ss_file) or not cache:
                 ss_json = fetch_second_screen_json(**cur_ss_id)
-                with open(ss_file, 'w') as f:
+                with open(ss_file, "w") as f:
                     json.dump(ss_json, f)
 
             if not isfile(widget_file) or not cache:
                 widget_json = fetch_widget_info_json(**cur_ss_id)
-                with open(widget_file, 'w') as f:
+                with open(widget_file, "w") as f:
                     json.dump(widget_json, f)
 
         except json.JSONDecodeError:
--- utils.py
+++ utils.py
@@ -5,18 +5,20 @@ from bs4 import BeautifulSoup
 def get_tournament_result_links(year):
 
     page = requests.get(
-        f'https://www.atptour.com/en/scores/results-archive?year={year}')
+        f"https://www.atptour.com/en/scores/results-archive?year={year}"
+    )
 
-    soup = BeautifulSoup(page.content, 'html.parser')
+    soup = BeautifulSoup(page.content, "html.parser")
 
-    all_results = soup.find_all('tr', attrs={'class': 'tourney-result'})
+    all_results = soup.find_all("tr", attrs={"class": "tourney-result"})
 
     # This is a bit tenuous as it relies on the class of the a element...
-    all_result_links = [x.find('a', attrs={'class':
-                                           'button-border'}).get('href') for x
-                        in all_results]
+    all_result_buttons = [
+        x.find("a", attrs={"class": "button-border"}) for x in all_results
+    ]
+    all_result_links = [x.get("href") for x in all_result_buttons if x is not None]
 
-    to_add = 'https://www.atptour.com'
+    to_add = "https://www.atptour.com"
 
     with_prefix = [to_add + x for x in all_result_links]
 
@@ -27,25 +29,29 @@ def get_second_screen_ids(tournament_result_link):
 
     page = requests.get(tournament_result_link)
 
-    soup = BeautifulSoup(page.content, 'html.parser')
+    soup = BeautifulSoup(page.content, "html.parser")
 
-    all_buttons = soup.find_all('a', attrs={'class': 'button-border'})
+    all_buttons = soup.find_all("a", attrs={"class": "button-border"})
 
-    all_links = [x.get('href') for x in all_buttons]
-    second_screen = [x for x in all_links if 'second-screen' in x]
+    all_links = [x.get("href") for x in all_buttons]
+    second_screen = [x for x in all_links if "second-screen" in x]
 
-    split_second_screen = [x.split('/') for x in second_screen]
+    split_second_screen = [x.split("/") for x in second_screen]
 
-    info = [{'year': x[-3], 'tour_id': x[-2], 'match_id': x[-1]}
-            for x in split_second_screen]
+    info = [
+        {"year": x[-3], "tour_id": x[-2], "match_id": x[-1]}
+        for x in split_second_screen
+    ]
 
     return info
 
 
 def fetch_second_screen_json(year, tour_id, match_id):
 
-    url = (f'https://www.atptour.com/-/ajax/HawkEyeSecondScreen/'
-           f'{year}/{tour_id}/{match_id}/')
+    url = (
+        f"https://www.atptour.com/-/ajax/HawkEyeSecondScreen/"
+        f"{year}/{tour_id}/{match_id}/"
+    )
 
     r = requests.get(url)
 
@@ -54,8 +60,10 @@ def fetch_second_screen_json(year, tour_id, match_id):
 
 def fetch_match_info_json(year, tour_id, match_id):
 
-    url = (f'https://www.atptour.com/-/ajax/HawkEyeSecondScreen/MatchStats/'
-           f'en/False/{year}/{tour_id}/{match_id}')
+    url = (
+        f"https://www.atptour.com/-/ajax/HawkEyeSecondScreen/MatchStats/"
+        f"en/False/{year}/{tour_id}/{match_id}"
+    )
 
     r = requests.get(url)
 
@@ -65,8 +73,10 @@ def fetch_match_info_json(year, tour_id, match_id):
 def fetch_widget_info_json(year, tour_id, match_id):
     # Includes spin etc.
 
-    url = (f'https://www.atptour.com/-/ajax/HawkEyeSecondScreen/'
-           f'{year}/{tour_id}/{match_id}/widget')
+    url = (
+        f"https://www.atptour.com/-/ajax/HawkEyeSecondScreen/"
+        f"{year}/{tour_id}/{match_id}/widget"
+    )
 
     r = requests.get(url)
 
